1e985d233e6998f964deb012b535d1539678074f
Layton Goeckeritz <laygoeckeritz@gmail.com>
2024-03-19 13:18:29 -0600

    getting server set up

---
+++ tweeter-server/src/lambda/LoginLambda.ts
@@ -0,0 +1,11 @@
+import { AuthenticateResponse, LoginRequest } from "tweeter-shared";
+import { UserService } from "../model/service/UserService";
+
+export class LoginLambda {
+    handler = async (event: LoginRequest): Promise<AuthenticateResponse> => {
+        let response = new AuthenticateResponse(
+            ...(await new UserService().login(event.username, event.password))
+        );
+        return response;
+    };
+}
--- tweeter-shared/src/index.ts
+++ tweeter-shared/src/index.ts
@@ -4,6 +4,8 @@ export { Status } from "./model/domain/Status";
 export { User } from "./model/domain/User";
 export { AuthToken } from "./model/domain/AuthToken";
 
-// All classes that should be avaialble to other modules need to exported here. export * does not work when 
+// All classes that should be avaialble to other modules need to exported here. export * does not work when
 // uploading to lambda. Instead we have to list each export.
 export { FakeData } from "./util/FakeData";
+export { LoginRequest } from "./model/net/Request";
+export { AuthenticateResponse } from "./model/net/Response";
