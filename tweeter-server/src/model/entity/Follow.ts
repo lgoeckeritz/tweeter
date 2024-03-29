f6d118400fd7345e512f28a95a41db1d933b12a7
Layton Goeckeritz <laygoeckeritz@gmail.com>
2024-03-29 10:45:31 -0600

    started creating daos

---
+++ tweeter-server/src/model/entity/Follow.ts
@@ -0,0 +1,36 @@
+export class Follow {
+    followerHandle: string;
+    followerName: string;
+    followeeHandle: string;
+    followeeName: string;
+
+    constructor(
+        followerHandle: string,
+        followerName: string,
+        followeeHandle: string,
+        followeeName: string
+    ) {
+        this.followerHandle = followerHandle;
+        this.followerName = followerName;
+        this.followeeHandle = followeeHandle;
+        this.followeeName = followeeName;
+    }
+
+    toString(): string {
+        return (
+            "Follow{" +
+            "follower handle='" +
+            this.followerHandle +
+            "'" +
+            ", follower name='" +
+            this.followerName +
+            "'" +
+            ", followee handle='" +
+            this.followeeHandle +
+            "'" +
+            ", followee name='" +
+            this.followeeName +
+            "'}"
+        );
+    }
+}
